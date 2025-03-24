07505dbd37f54312a7e8c91db46f2592cc1cebee
SahilShameerDev <smsahilms346@gmail.com>
2025-03-24 12:07:55 +0530

    Add funtionality to emojis and storevalues using hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:mindtrack/pages/home_pages.dart';
 import 'package:mindtrack/pages/screen_time.dart';
 import 'package:mindtrack/pages/unlock_count.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   runApp(const MyApp());
 }
 
@@ -14,12 +16,41 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-      routes: {
-        '/screen-time': (context) =>  ScreenTimePage(platform: const MethodChannel('com.example.screen_time_tracker/screen_time'),),
-        '/unlock-count': (context) => UnlockCountPage(platform: const MethodChannel('com.example.screen_time_tracker/screen_time'),),
+    return FutureBuilder(
+      future: Hive.openBox<int>('moodBox'), // Open the Hive box
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return MaterialApp(
+              home: Scaffold(
+                body: Center(
+                  child: Text('Error initializing Hive: ${snapshot.error}'),
+                ),
+              ),
+            );
+          } else {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: const HomePage(),
+              routes: {
+                '/screen-time': (context) => ScreenTimePage(
+                      platform: const MethodChannel('com.example.screen_time_tracker/screen_time'),
+                    ),
+                '/unlock-count': (context) => UnlockCountPage(
+                      platform: const MethodChannel('com.example.screen_time_tracker/screen_time'),
+                    ),
+              },
+            );
+          }
+        } else {
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(), // Show a loading indicator while Hive initializes
+              ),
+            ),
+          );
+        }
       },
     );
   }
